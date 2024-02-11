5536661f800dea99091142e03e2b8d3312b36b17
Juan Carlos Herrera Blandon <desarrollo.java@ada.co>
2024-02-11 18:45:31 -0500

    Se realiza cambio estructura de carpetas y actualizacion de informacion contacto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { RespuestaSuscripcionOfertaComponent } from './components/respuesta-suscripcion-oferta/respuesta-suscripcion-oferta.component';
-import { HomeComponent } from './components/home/home.component';
+import { RespuestaSuscripcionOfertaComponent } from './pages/respuesta-suscripcion-oferta/respuesta-suscripcion-oferta.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { HeaderComponent } from '../header/header.component';
-import { ObjetivosGeneralesComponent } from '../objetivos-generales/objetivos-generales.component';
-import { BoletinOfertasComponent } from '../boletin-ofertas/boletin-ofertas.component';
-import { ProductosServiciosComponent } from '../productos-servicios/productos-servicios.component';
-import { PieDePaginaComponent } from '../pie-de-pagina/pie-de-pagina.component';
+import { HeaderComponent } from '../../components/header/header.component';
+import { ObjetivosGeneralesComponent } from '../../components/objetivos-generales/objetivos-generales.component';
+import { BoletinOfertasComponent } from '../../components/boletin-ofertas/boletin-ofertas.component';
+import { ProductosServiciosComponent } from '../../components/productos-servicios/productos-servicios.component';
+import { PieDePaginaComponent } from '../../components/pie-de-pagina/pie-de-pagina.component';
 import { RespuestaSuscripcionOfertaComponent } from '../respuesta-suscripcion-oferta/respuesta-suscripcion-oferta.component';
 
 @Component({
